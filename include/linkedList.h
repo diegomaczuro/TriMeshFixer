115b88ec537c17dad1d04d5dd892a076d144f301
shido <banas49@mail.ru>
2023-12-06 12:17:23 +0100

    simple structure for double linked list

--- include/linkedList.h
+++ include/linkedList.h
@@ -8,8 +8,19 @@ class Node
 public:
     
     int node_data;
-
+    // Empty constructor
+    Node();
+    // Isolated node
+    Node(int data);
+    // Insert Node between nodes p and n
+    Node(Node *p, int data, Node *n);
+    // Destroy a link to node
     ~Node();
+    // Get data of node
+    Node *get_prev(){return prev_node;}
+    Node *get_next(){return next_node;}
+    int get_node_data(){return node_data;}
+
     Node *prev_node;
     Node *next_node;    
 };
@@ -19,12 +30,14 @@ class DoubleLinkedList
 public:
     Node *head;
     Node *tail;
+    int length_of_list;
 
     // Constructor: head and tail is linked to NULL
     DoubleLinkedList();
     //add_front: Adds a new node in the beginning of list
-    void addAtFront(int new_node);
+    void addToFront(int new_node);
     //add_after: Adds a new node after another node
+    void addAfterNode(Node *node, int new_data);
     //add_before: Adds a new node before another node
     //add_end: Adds a new node in the end of list
     //delete: Removes the node
--- src/linkedList.cpp
+++ src/linkedList.cpp
@@ -1,12 +1,38 @@
 #include <linkedList.h>
 
+Node::Node()
+{
+    prev_node = NULL;
+    next_node = NULL;
+};
+
+Node::Node(int data)
+{
+    node_data = data;
+    prev_node = NULL;
+    next_node = NULL;
+};
+
+Node::Node(Node *p, int data, Node *n)
+{
+    node_data = data;
+
+    if (p != NULL) 
+    {
+        p->next_node = this;
+        std::cout << p->next_node << std::endl;
+    }
+    if (n != NULL) n->prev_node = this;    
+};
+
 DoubleLinkedList::DoubleLinkedList()
 {
     head = NULL;
     tail = NULL;
+    length_of_list = 0;
 };
 
-void DoubleLinkedList::addAtFront(int new_data)
+void DoubleLinkedList::addToFront(int new_data)
 {
     Node *temp_node = new Node();
     temp_node->node_data = new_data;
@@ -19,15 +45,37 @@ void DoubleLinkedList::addAtFront(int new_data)
         head->prev_node = temp_node;
 
     head = temp_node;
+};
 
+void DoubleLinkedList::addAfterNode(Node *current_node, int new_data)
+{
+    Node *temp_node = new Node();
+    temp_node->node_data = new_data;
+    temp_node->prev_node = current_node;
+    temp_node->next_node = current_node->next_node;
+    //my addition
+    if (current_node->next_node != NULL)
+        current_node->next_node->prev_node = temp_node;
+
+    current_node->next_node = temp_node;
+
+    if (current_node->next_node == NULL)
+        tail = temp_node;
 };
 
 
 int main()
 {   
-   
-    DoubleLinkedList linked_list;
-    linked_list.addAtFront(10);
+    Node *node1 = new Node(0);
+    Node *node2 = new Node(1);
+    Node *node3 = new Node(NULL, 2, NULL);
+    Node *node4 = new Node(node1, 2, NULL);
+    Node *node5 = new Node(node4, 3, node2);
+    /*DoubleLinkedList linked_list;
+    linked_list.addToFront(10);
+    linked_list.addToFront(100);
+
+    linked_list.addAfterNode(50)*/
     std::cout << "Start Linked List: " << std::endl;
 
 
